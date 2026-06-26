157a9ad3d5bb32576c07d72abd704da623f1a243
Al Malik Dzulkipli <alik@Als-MacBook-Pro.local>
2026-06-27 04:23:25 +0930

    started load instructions

--- src/CPU.h
+++ src/CPU.h
@@ -1,5 +1,6 @@
 #include <cstdint>
 #include <vector>
+#include <unordered_map>
 
 namespace gb_emulator {
     template <typename ConcreteMem>
@@ -26,6 +27,7 @@ namespace gb_emulator {
         }
     };
 
+    template <typename Memory>
     class CPU {
     private:
         std::uint8_t a_;
@@ -39,6 +41,47 @@ namespace gb_emulator {
         std::uint16_t pc_;
         std::uint16_t sp_;
 
+        Memory& mem_;
+
+        void tick(int cycles) {}
+
+        std::uint8_t bus_read(std::uint16_t address) {
+            std::uint8_t val = mem_.read(address);
+            tick(4);
+            return val;
+        }
+
+        std::uint8_t fetch8() {
+            return bus_read(pc_++);
+        }
+
+        enum class r8 {
+            a, b, c, d, e, f, h, l
+        };
+
+        std::uint8_t read_r8(r8 source) {
+            switch(source) {
+                case r8::a: return a();
+                case r8::b: return b();
+                case r8::c: return c();
+                case r8::d: return d();
+                case r8::e: return e();
+                case r8::f: return f();
+                case r8::h: return h();
+                case r8::l: return l();
+            }
+        }
+
+        void write_r8(r8 dest, std::uint8_t val) {
+            switch(dest) {
+                case r8::a: a(val);
+                break;
+                case r8::b: b(val);
+                break;
+
+            }
+        }
+
     public:
         std::uint8_t a() const {return a_;}
         std::uint8_t b() const {return b_;}
@@ -48,12 +91,14 @@ namespace gb_emulator {
         std::uint8_t f() const {return f_;}
         std::uint8_t h() const {return h_;}
         std::uint8_t l() const {return l_;}
+
         std::uint16_t pc() const {return pc_;}
         std::uint16_t sp() const {return sp_;}
         std::uint16_t af() const {return (a_ << 8) | f_;}
         std::uint16_t bc() const {return (b_ << 8) | c_;}
         std::uint16_t de() const {return (d_ << 8) | e_;}
         std::uint16_t hl() const {return (h_ << 8) | l_;}
+
         void a(std::uint8_t val) {a_ = val;}
         void b(std::uint8_t val) {b_ = val;}
         void c(std::uint8_t val) {c_ = val;}
@@ -66,6 +111,7 @@ namespace gb_emulator {
         void l(std::uint8_t val) {l_ = val;}
         void pc(std::uint16_t val) {pc_ = val;}
         void sp(std::uint16_t val) {sp_ = val;}
+
         void af(std::uint16_t val) {
             a_ = static_cast<uint8_t>(val >> 8);
             f(static_cast<uint8_t>(val));
@@ -82,10 +128,32 @@ namespace gb_emulator {
             h_ = static_cast<uint8_t>(val >> 8);
             l_ = static_cast<uint8_t>(val);
         }
+
         bool flag_z() const {return f_ & 0x80;}
         bool flag_n() const {return f_ & 0x40;}
         bool flag_h() const {return f_ & 0x20;}
         bool flag_c() const {return f_ & 0x10;}
-        CPU() : a_{0}, b_{0}, c_{0}, d_{0}, e_{0}, f_{0}, h_{0}, l_{0}, pc_{0}, sp_{0} {}
+
+        CPU(Memory& mem) : a_{0}, b_{0}, c_{0}, d_{0}, e_{0}, f_{0}, h_{0}, l_{0}, pc_{0}, sp_{0}, mem_{mem} {}
+
+        // enum class Op8 {
+        //     a, b, c, d, e, h, l,
+        //     imm8,
+        //     mem_bc, mem_de, mem_hl,
+        //     mem_hl_inc, mem_hl_dec,
+        //     mem_imm16,
+        //     high_c,
+        //     high_n
+        // };
+
+        // enum class Op16 {
+        //     bc, de, hl, sp,
+        //     af,
+        //     imm16
+        // };
+
+
+
+
     };
 }
