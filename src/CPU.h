a27b24a501d182d80f85ecf2ce9ac91e047182d7
Al Malik Dzulkipli <alik@Als-MacBook-Pro.local>
2026-06-30 21:20:55 +0930

    partway through flag operations

--- src/CPU.h
+++ src/CPU.h
@@ -386,8 +386,8 @@ namespace gb_emulator {
 
         std::uint8_t inc_r8_set_flags(std::uint8_t data) {
             std::uint8_t lo = data & 0xF;
-            std::uint8_t new_flag = 0x80*(data + 1 == 0) |
-                0x40*(data + 1 == 0) |
+            std::uint8_t new_flag = 0x80*(((data + 1) & 0xFF) == 0) |
+                0x40*0 |
                 0x20*(lo + 1 > 0xF) |
                 0x10*flag_c();
             return new_flag;
@@ -414,10 +414,10 @@ namespace gb_emulator {
         std::uint8_t sbc_a_r8_set_flags(std::uint8_t a_data, std::uint8_t data, bool carry) {
             std::uint8_t lo_a = a_data & 0xF;
             std::uint8_t lo_data = data & 0xF;
-            std::uint8_t new_flag = 0x8*(a_data - data - carry == 0) |
-                0x4 |
-                0x2*(lo_a < lo_data + carry) |
-                0x1*(a_data < data + carry);
+            std::uint8_t new_flag = 0x80*(a_data - data - carry == 0) |
+                0x40 |
+                0x20*(lo_a < lo_data + carry) |
+                0x10*(a_data < data + carry);
             return new_flag;
         }
 
@@ -484,10 +484,198 @@ namespace gb_emulator {
             f(new_f);
         }
 
+        std::uint8_t add_hl_r16_set_flags(std::uint16_t hl_data, std::uint16_t data) {
+            std::uint16_t hl_data_up_to_bit_11 = hl_data & 4095;
+            std::uint16_t data_up_to_bit_11 = data & 4095;
+            std::uint8_t new_flag = 0x80*flag_z() |
+                0x40*0 |
+                0x20*(hl_data_up_to_bit_11 + data_up_to_bit_11 >= (1 << 12)) |
+                0x10*(hl_data + data >= (1 << 16));
+            return new_flag;
+        }
+
+        // ADD HL,r16
+        void add_hl_r16(r16 reg) {
+            tick(4);
+            std::uint16_t data = read_r16(reg);
+            std::uint16_t hl_data = hl();
+            std::uint8_t new_f = add_hl_r16_set_flags(hl_data, data);
+            hl(data + hl_data);
+            f(new_f);
+        }
+
+        // DEC r16
+        void dec_r16(r16 reg) {
+            tick(4);
+            std::uint16_t data = read_r16(reg);
+            write_r16(reg, data-1);
+        }
+
+        // INC r16
+        void inc_r16(r16 reg) {
+            tick(4);
+            std::uint16_t data = read_r16(reg);
+            write_r16(reg, data+1);
+        }
+
+        std::uint8_t and_a_r8_set_flags(std::uint8_t a_data, std::uint8_t data) {
+            std::uint8_t new_flag = 0x80*((a_data & data) == 0) |
+                0x40*0 |
+                0x20 |
+                0x10*0;
+            return new_flag;
+        }
+
+        // AND A,r8
+        void and_a_r8(r8 source) {
+            std::uint8_t data = read_r8(source);
+            std::uint8_t a_data = a();
+            std::uint8_t new_f = and_a_r8_set_flags(a_data, data);
+            a(data & a_data);
+            f(new_f);
+        }
+
+        // AND A,[HL]
+        void and_a_hl_mem() {
+            std::uint8_t data = bus_read(hl());
+            std::uint8_t a_data = a();
+            std::uint8_t new_f = and_a_r8_set_flags(a_data, data);
+            a(data & a_data);;
+            f(new_f);
+        }
+
+        // AND A,n8
+        void and_a_n8() {
+            std::uint8_t data = fetch8();
+            std::uint8_t a_data = a();
+            std::uint8_t new_f = and_a_r8_set_flags(a_data, data);
+            a(data & a_data);;
+            f(new_f);
+        }
+
         // CPL
         void cpl() {
             a(~a());
             f(0x80*flag_z() | 0x40 | 0x20 | 0x10*flag_c());
+        }
+
+        std::uint8_t or_a_r8_set_flags(std::uint8_t a_data, std::uint8_t data) {
+            std::uint8_t new_flag = 0x80*((a_data | data) == 0);
+            return new_flag;
+        }
+
+        // OR A,r8
+        void or_a_r8(r8 source) {
+            std::uint8_t data = read_r8(source);
+            std::uint8_t a_data = a();
+            std::uint8_t new_f = or_a_r8_set_flags(a_data, data);
+            a(a_data | data);
+            f(new_f);
+        }
+
+        // OR A,[HL]
+        void or_a_hl_mem() {
+            std::uint8_t data = bus_read(hl());
+            std::uint8_t a_data = a();
+            std::uint8_t new_f = or_a_r8_set_flags(a_data, data);
+            a(a_data | data);
+            f(new_f);
+        }
+
+        // OR A,n8
+        void or_a_n8() {
+            std::uint8_t data = fetch8();
+            std::uint8_t a_data = a();
+            std::uint8_t new_f = or_a_r8_set_flags(a_data, data);
+            a(a_data | data);
+            f(new_f);
+        }
+
+        std::uint8_t xor_a_r8_set_flags(std::uint8_t a_data, std::uint8_t data) {
+            std::uint8_t new_flag = 0x80*((a_data ^ data) == 0);
+            return new_flag;
+        }
+
+        // XOR A,r8
+        void xor_a_r8(r8 source) {
+            std::uint8_t data = read_r8(source);
+            std::uint8_t a_data = a();
+            std::uint8_t new_f = xor_a_r8_set_flags(a_data, data);
+            a(a_data ^ data);
+            f(new_f);
+        }
+
+        // XOR A,[HL]
+        void xor_a_hl_mem() {
+            std::uint8_t data = bus_read(hl());
+            std::uint8_t a_data = a();
+            std::uint8_t new_f = xor_a_r8_set_flags(a_data, data);
+            a(a_data ^ data);
+            f(new_f);
+        }
+
+        // XOR A,n8
+        void xor_a_n8() {
+            std::uint8_t data = fetch8();
+            std::uint8_t a_data = a();
+            std::uint8_t new_f = xor_a_r8_set_flags(a_data, data);
+            a(a_data ^ data);
+            f(new_f);
+        }
+
+        enum class u3 {
+            u_0, u_1, u_2, u_3, u_4, u_5, u_6, u_7
+        };
+
+        int u3_to_int(u3 bit) {
+            switch(bit) {
+                case u3::u_0: return 0;
+                case u3::u_1: return 1;
+                case u3::u_2: return 2;
+                case u3::u_3: return 3;
+                case u3::u_4: return 4;
+                case u3::u_5: return 5;
+                case u3::u_6: return 6;
+                case u3::u_7: return 7;
+                default: std::unreachable();
+            }
+        }
+
+        std::uint8_t bit_u3_r8_set_flags(int bit_num, std::uint8_t data) {
+            std::uint8_t new_flag = 0x80*((data & (1 << bit_num)) == 0) | 0x20;
+            return new_flag;
+        }
+
+        // BIT u3,r8
+        void bit_u3_r8(u3 bit, r8 reg) {
+            int bit_num = u3_to_int(bit);
+            std::uint8_t data = read_r8(reg);
+            std::uint8_t new_f = bit_u3_r8_set_flags(bit_num, data);
+            f(new_f);
+        }
+
+        // BIT u3,[HL]
+        void bit_u3_hl_mem(u3 bit) {
+            int bit_num = u3_to_int(bit);
+            std::uint8_t data = bus_read(hl());
+            std::uint8_t new_f = bit_u3_r8_set_flags(bit_num, data);
+            f(new_f);
+        }
+
+        // RES u3,r8
+        void res_u3_r8(u3 bit, r8 reg) {
+            std::uint8_t data = read_r8(reg);
+            int bit_num = u3_to_int(bit);
+            data &= ~(1 << bit_num);
+            write_r8(reg, data);
+        }
+
+        // RES u3,[HL]
+        void res_u3_hl_mem(u3 bit) {
+            std::uint8_t data = bus_read(hl());
+            int bit_num = u3_to_int(bit);
+            data &= ~(1 << bit_num);
+            write8(hl(), data);
         }
 
         // LD SP,n16
